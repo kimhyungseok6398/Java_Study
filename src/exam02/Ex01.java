08b7e4bc0280fdf6a0fd7d90adb827863e9b5319
kimhyungseok6398 <gudtjr6398@naver.com>
2023-10-15 17:34:48 +0900

    java 개념정리

---
+++ src/exam02/Ex01.java
@@ -0,0 +1,11 @@
+package exam02;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int num; // 변수 선언 메모리 공간 생성
+        num = 10; // 메모리 공간에 저장 ( 초기화 )
+
+        int num2 = 10;
+        System.out.println(num2);
+    }
+}
---
+++ src/exam02/Ex03.java
@@ -0,0 +1,10 @@
+package exam02;
+
+public class Ex03 {
+    public static void main(String[] args) {
+    char ch = 'A';
+    System.out.println(ch);
+    System.out.println(ch+1);
+    // 알파벳 문자도 연산가능
+    }
+}
