76907df9c85cee63696dde6813f62dabfec5adfb
kimhyungseok6398 <gudtjr6398@naver.com>
2023-10-16 21:34:18 +0900

    변수명  복습완료

---
+++ src/exam02/Ex04.java
@@ -0,0 +1,14 @@
+package exam02;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        double num1 = 100.123;
+       //float num2 = 100.123; // 뒤에 F를 넣어줘야함
+        float num2 = 100.123f;
+
+        boolean result = true; // 연산자에서 주로 쓰임
+        boolean result2 = false;
+
+        // 기본 자료형 - 숫자를 저장하는 자료형
+    }
+}
