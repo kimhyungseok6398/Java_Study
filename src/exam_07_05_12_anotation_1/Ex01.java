a340775053a1186a1ef85ac038319fd549a869e1
kimhyungseok6398 <gudtjr6398@naver.com>
2023-12-05 15:13:19 +0900

    어노테이션 복습

---
+++ src/exam_07_05_12_anotation_1/Ex01.java
@@ -0,0 +1,12 @@
+package exam_07_05_12_anotation_1;
+
+import java.lang.annotation.Annotation;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Class cls = Book.class;
+        //Annotation anno = cls.getAnnotation(MyAnno.class);
+
+
+    }
+}
